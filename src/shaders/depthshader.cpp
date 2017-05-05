d51d18ca3686c7dd7d960c9bc69ce269f94eb7a0
u104031 <u104031@DS.UPF.EDU>
2017-05-05 17:39:09 +0200

    Nova classe de la carpeta shaders, és molt semblan els altres, peró s'ha hagut de crear de nou

---
+++ src/shaders/depthshader.cpp
@@ -0,0 +1,19 @@
+﻿#include "depthshader.h"
+#include "../core/utils.h"
+
+DepthShader::DepthShader(Vector3D color_, double maxDist_, Vector3D bgColor_) : Shader(bgColor_), maxDist(maxDist_), color(color_)
+{}
+
+Vector3D DepthShader::computeColor(const Ray & r, const std::vector<Shape*>& objList, const std::vector<PointLightSource>& lsList) const
+{
+	Intersection its;
+
+	if (Utils::getClosestIntersection(r, objList, its))
+	{
+		return color * (1.0 - r.maxT/maxDist);
+	}
+	else
+	{
+		return bgColor;
+	}
+}
