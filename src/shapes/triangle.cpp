8d024401f77e386eed810e1baec02c041b4d60ab
Aurel Ioan Patrutiu <aureldevices@hotmail.com>
2017-05-19 08:04:57 +0200

    Class triangle Created

---
+++ src/shapes/triangle.cpp
@@ -0,0 +1,31 @@
+#include "triangle.h"
+
+Triangle::Triangle(const Vector3D a_, const Vector3D b_, const Vector3D c_, Material * material_):Shape(Matrix4x4(),material_), aWorld(a_),bWorld(b_),cWorld(c_)
+{
+	nWorld = cross((cWorld - aWorld), (bWorld - aWorld));
+}
+
+Vector3D Triangle::getNormalWorld(const Vector3D & pt_world) const
+{
+	return Vector3D();
+}
+
+bool Triangle::rayIntersect(const Ray & ray, Intersection & its) const
+{
+	return false;
+}
+
+bool Triangle::rayIntersectP(const Ray & ray) const
+{
+	return false;
+}
+
+std::string Triangle::toString() const
+{
+	return std::string();
+}
+
+std::ostream & operator<<(std::ostream & out, const Triangle & s)
+{
+	// TODO: insertar una instrucción return aquí
+}
