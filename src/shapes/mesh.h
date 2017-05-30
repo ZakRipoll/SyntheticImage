72ce7440c85a4e5aa64de1ff47ab0e836b6b3181
u104031 <u104031@DS.UPF.EDU>
2017-05-30 19:45:08 +0200

    mesh.h i cpp

---
+++ src/shapes/mesh.h
@@ -0,0 +1,28 @@
+/*  by Javi Agenjo 2013 UPF  javi.agenjo@gmail.com
+	The Mesh contains the info about how to render a mesh and also how to parse it from a file.
+*/
+
+#ifndef MESH_H
+#define MESH_H
+
+#include <vector>
+#include <iostream>
+#include "../core/vector3d.h"
+
+class Mesh
+{
+public:
+	std::vector< Vector3D > vertices; //here we store the vertices
+	std::vector< Vector3D > normals;	 //here we store the normals
+	//std::vector< Vector2 > uvs;	 //here we store the texture coordinates, OPCIONAL XD
+
+	Mesh();
+	void clear();
+
+	void createPlane(float size);
+	bool loadOBJ(const char* filename);
+};
+
+
+
+#endif
